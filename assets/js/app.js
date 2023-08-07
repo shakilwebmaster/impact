a275e7cf4e6f961a89ea42123b017776c0b7e894
shakilwebmaster <mdshakil102036@gmail.com>
2023-08-08 01:31:14 +0600

    responsive design, button change

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,26 +1,31 @@
 // card show function
-const cardOne = document.querySelector('#product-card-one');
-const cardTwo = document.querySelector('#product-card-two');
+const cartOne = document.querySelector('#product-card-one');
+const cartTwo = document.querySelector('#product-card-two');
 const cardBadge = document.querySelector('#card-badge');
+const showProduct = document.querySelector('.product-card-container');
 
-const showCardOne = () =>{
-    cardOne.style.display = "block";
-    cardTwo.style.display = "none";
+const showCartOne = () =>{
+    cartOne.style.display = "block";
+    cartTwo.style.display = "none";
     cardBadge.style.display = "block";
+    showProduct.style.transform ="translateX(0%)";
+    showProduct.style.transition = "all 0.5s ease-in-out"
 }
-const showCardTwo = () =>{
-    cardTwo.style.display = "block";
-    cardOne.style.display = "none";
+const showCartTwo = () =>{
+    cartTwo.style.display = "block";
+    cartOne.style.display = "none";
     cardBadge.style.display = 'none';
+    showProduct.style.transform = "translateX(-50%)";
+    showProduct.style.transition = "all 0.5s ease-in-out"
 }
 const showCardToggle = () =>{   
-    cardOne.style.display = (cardOne.style.display === "block") ? "none" : "block";
-    cardTwo.style.display = (cardTwo.style.display === "none") ? "block"  : "none";
-    cardBadge.style.display = (cardTwo.style.display === "block") ? cardBadge.style.display = "none" : "block";
+    cartOne.style.display = (cartOne.style.display === "block") ? "none" : "block";
+    cartTwo.style.display = (cartTwo.style.display === "none") ? "block"  : "none";
+    cardBadge.style.display = (cartTwo.style.display === "block") ? cardBadge.style.display = "none" : "block";
 }
 
 // product color choice 
-    const colorButtons = document.querySelectorAll('.color');
+    const colorButtons = document.querySelectorAll('.plica-color .color');
     const colorImages = {
     gray: "./assets/img/gray.jpg",
     light:"./assets/img/light.webp",
@@ -28,15 +33,16 @@ const showCardToggle = () =>{
     green:"./assets/img/green.webp"
 };
 
-function showColorImage(event) {
+const  showColorImage = (event) =>{
     const colorName = event.target.dataset.color;
     const imageUrl = colorImages[colorName];
     const colorImage = document.querySelector("#colorImage");
     colorImage.src = imageUrl;
 
+
+
     // Remove active class from all color buttons
     colorButtons.forEach(button => button.classList.remove('active'));
-
     // Add active class to the clicked color button
     event.target.classList.add('active');
   }
@@ -44,3 +50,22 @@ function showColorImage(event) {
   colorButtons.forEach((button) => {
     button.addEventListener('click', showColorImage)
   });
+
+// quick buy button show
+const quickBuyShowContainer = document.querySelector('.shop-the-look_products');
+const quickBuyButtons = document.querySelectorAll('.product-card_quick-buy');
+
+quickBuyShowContainer.addEventListener('mouseover', () => {
+    quickBuyButtons.forEach(button => {
+        button.style.display = "block";
+        button.style.transition = "all 0.5s ease-in";
+    });
+});
+
+quickBuyShowContainer.addEventListener('mouseout', () => {
+    quickBuyButtons.forEach(button => {
+        button.style.display = "none";
+        button.style.transition = "all 0.5s ease-out";
+    });
+});
+
