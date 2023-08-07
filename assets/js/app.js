75f7653b0cf06e439b1432c58e6197ef0ca550a8
shakilwebmaster <mdshakil102036@gmail.com>
2023-08-07 11:51:39 +0600

    next button and previous button add

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,13 +1,46 @@
+// card show function
 const cardOne = document.querySelector('#product-card-one');
 const cardTwo = document.querySelector('#product-card-two');
-const cardBadge = document.querySelector('#card-badge')
+const cardBadge = document.querySelector('#card-badge');
 
 const showCardOne = () =>{
     cardOne.style.display = "block";
-    cardTwo.style.display = "none"
+    cardTwo.style.display = "none";
+    cardBadge.style.display = "block";
 }
 const showCardTwo = () =>{
     cardTwo.style.display = "block";
-    cardOne.style.display = "none"
+    cardOne.style.display = "none";
     cardBadge.style.display = 'none';
 }
+const showCardToggle = () =>{   
+    cardOne.style.display = (cardOne.style.display === "block") ? "none" : "block";
+    cardTwo.style.display = (cardTwo.style.display === "none") ? "block"  : "none";
+    cardBadge.style.display = (cardTwo.style.display === "block") ? cardBadge.style.display = "none" : "block";
+}
+
+// product color choice 
+    const colorButtons = document.querySelectorAll('.color');
+    const colorImages = {
+    gray: "./assets/img/gray.jpg",
+    light:"./assets/img/light.webp",
+    lightRed:"./assets/img/lightRed.webp",
+    green:"./assets/img/green.webp"
+};
+
+function showColorImage(event) {
+    const colorName = event.target.dataset.color;
+    const imageUrl = colorImages[colorName];
+    const colorImage = document.querySelector("#colorImage");
+    colorImage.src = imageUrl;
+
+    // Remove active class from all color buttons
+    colorButtons.forEach(button => button.classList.remove('active'));
+
+    // Add active class to the clicked color button
+    event.target.classList.add('active');
+  }
+
+  colorButtons.forEach((button) => {
+    button.addEventListener('click', showColorImage)
+  });
